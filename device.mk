215fdc0631ab171e1d4a933a37eeb89d026a3c45
BasGame1 <160914105+BasGame1@users.noreply.github.com>
2025-06-16 22:59:57 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.zuma
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.zuma \
     libgptutils \
     libz \
