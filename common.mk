b872100b83c41e2770ca029830dfd31f7bc97701
BasGame1 <160914105+BasGame1@users.noreply.github.com>
2025-06-17 09:36:56 +0200

    Create common.mk

---
+++ common.mk
@@ -0,0 +1,13 @@
+# vendor/omni/config/common.mk
+
+# Inherit some common Omni stuff
+$(call inherit-product, vendor/omni/build/omni.mk)
+
+# Enable debugging
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.debuggable=1 \
+    persist.sys.usb.config=mtp,adb
+
+# Include any additional device-independent settings here
+
+# Add more global settings as needed
